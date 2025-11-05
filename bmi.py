790fe0205213053bb987e4f282452d12782ef663
MoeThanzin <mthanzin.24@ichat.sp.edu.sg>
2025-11-05 09:38:05 +0800

    modified w3

--- bmi.py
+++ bmi.py
@@ -5,9 +5,12 @@ def calculate_bmi(height,weight):
     print("BMI= "+str(bmi))
     if bmi<18.5:
         print("Underweight")
+        return(-1)
     elif bmi>=18.5 and bmi<=25.0:
         print("Normal weight")
+        return(0)
     else:
         print("Overweight")
+        return(1)
 
 calculate_bmi(weight=57, height=1.6)
